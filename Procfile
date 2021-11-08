cfe06a379b47d1303f33916039032bbfb49fdc10
Kirill Nazarov <kirillnazarov1928@gmail.com>
2021-11-08 07:55:38 +0300

    Cleaning up for deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn gamification.gamification.wsgi --log-file -
--- gamification/gamification/settings.py
+++ gamification/gamification/settings.py
@@ -70,6 +70,8 @@ WSGI_APPLICATION = 'gamification.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+
+"""
 DATABASES = {
     'default': {
         'ENGINE': os.environ.get("SQL_ENGINE"),
@@ -80,6 +82,7 @@ DATABASES = {
         "PORT": os.environ.get("SQL_PORT"),
     }
 }
+"""
 
 
 # Password validation
@@ -130,3 +133,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
