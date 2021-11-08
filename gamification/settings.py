908230a71e06fd6927c1f5ecfc545cd87e882735
Kirill Nazarov <kirillnazarov1928@gmail.com>
2021-11-08 09:23:15 +0300

    updated settings again

--- gamification/settings.py
+++ gamification/settings.py
@@ -70,7 +70,13 @@ WSGI_APPLICATION = 'gamification.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'mydatabase',
+    }
+}
+
 """
 DATABASES = {
     'default': {
